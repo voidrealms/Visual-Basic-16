a9209a456b67bad7cf5726b8e1d8b8fdb63f6e83
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:40:04 -0400

    First commit

---
+++ VB 16/ConsApp Queue 16/Module1.vb
@@ -0,0 +1,21 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'queue - first in is first out. like a bank line
+        Dim Line As Queue(Of String) = New Queue(Of String)
+
+        Line.Enqueue("bob")
+        Line.Enqueue("mary")
+        Line.Enqueue("chad")
+
+
+        Console.WriteLine("count =" & Line.Count)
+        Console.WriteLine(Line.Dequeue)
+        Console.WriteLine("count =" & Line.Count)
+
+
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: Visual Basic 16 - Queue
+Published: 7/11/2010 12:05:57 PM
+Link: https://www.youtube.com/watch?v=D0TOEBu5XeM
+
+Description: Learn about  the FIFO (First in First out) Queue class
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
